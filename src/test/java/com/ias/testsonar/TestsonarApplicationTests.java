416616ad27549157d4f1e2223e511902a4443877
Alex Lee <iasandcb@gmail.com>
2021-11-02 12:27:55 +0900

    Initial commit

---
+++ src/test/java/com/ias/testsonar/TestsonarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ias.testsonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsonarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
